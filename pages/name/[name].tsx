4a68a46e6f9f9098ed6b39d721f9028261e13138
jhosua.tsakana <jhosua.tsakana@gmail.com>
2022-07-29 08:15:55 -0500

    add: ISG to search by name or id

--- pages/name/[name].tsx
+++ pages/name/[name].tsx
@@ -116,17 +116,29 @@ export const getStaticPaths: GetStaticPaths = async (ctx) => {
 
   return {
     paths: pokemonNames.map((name) => ({ params: { name } })),
-    fallback: false,
+    fallback: "blocking",
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { name } = params as { name: string };
 
+  const pokemon = await getPokemonInfo(name.toLocaleLowerCase());
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(name),
+      pokemon,
     },
+    revalidate: 86400, // one day
   };
 };
 
